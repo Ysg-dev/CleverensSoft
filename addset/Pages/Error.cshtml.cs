cdf51838107166bf30b87597ca0735a39e2c855b
Ysg-dev <dzhuraev977@gmail.com>
2025-04-04 13:18:33 +0300

    bugfix

--- addset/Pages/Error.cshtml.cs
+++ addset/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace addset.Pages
+namespace Composition.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
